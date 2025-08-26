02ae8c7a6eec74b574aee04d0500dd903a92d909
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2025-08-26 08:14:57 +0530

    solution restructuring for introducing tests

---
+++ tests/DigitalPetApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace DigitalPetApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Sample_test()
+        {
+            Assert.True(true);
+        }
+    }
+}
